5454cba46ceb3a66e671aa832c5366911f5868c5
Jeff Tipps <jeff.tipps@ni.com>
2023-02-22 18:03:41 -0600

    crudely making membership thread safe

--- yachat/src/main/java/common/Membership.java
+++ yachat/src/main/java/common/Membership.java
@@ -18,11 +18,11 @@ public class Membership {
 
     public Membership() {}
 
-    public void Add(Member member) {
+    public synchronized void Add(Member member) {
         members.add(member);
     }
 
-    public boolean ScreenNameExists(String screenName){
+    public synchronized boolean ScreenNameExists(String screenName){
         for (Member member : members){
             if (member.nameEquals(screenName)){
                 return true;
@@ -31,11 +31,11 @@ public class Membership {
         return false;
     }
 
-    public void Remove(String memberName) {
+    public synchronized void Remove(String memberName) {
         members.removeIf(n -> (n.nameEquals(memberName)));
     }
 
-    public void Send(String command, DatagramSocket udpSocket) throws IOException {
+    public synchronized void Send(String command, DatagramSocket udpSocket) throws IOException {
         for (Member member : members) {
             byte[] sendData = new byte[1024]; 
             sendData = command.getBytes();
@@ -45,7 +45,7 @@ public class Membership {
         }
     }
 
-    public String acceptMessage(String myName) {
+    public synchronized String acceptMessage(String myName) {
         String acceptString = "";
         String chatroomMsg = " is in the chatroom\n";
         String acceptMsg = " accepted to the chatroom\n";
@@ -60,7 +60,7 @@ public class Membership {
     }
 
     @Override
-    public String toString() {
+    public synchronized String toString() {
         String membershipString = "";
         for (Member member : members) {
             membershipString += member.screenName + " " + member.ipAddress + " " + Integer.toString(member.udpPort) + ":";
