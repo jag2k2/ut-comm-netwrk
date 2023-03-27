43091280501287780f8014c492005393c364489a
Jeff Tipps <jeff.tipps@ni.com>
2023-03-27 12:38:18 -0500

    tweaked stuff

--- congestion_ctrl/dumbell.py
+++ congestion_ctrl/dumbell.py
@@ -16,24 +16,21 @@ def simpleTest():
     topo = DumbellTopo()
     net = Mininet(topo)
     net.start()
-    # print( "Dumping host connections" )
-    # dumpNodeConnections(net.hosts)
-    # print( "Testing network connectivity" )
-    # net.pingAll()
+
     h1, h2, h3, h4 = net.get('h1', 'h2', 'h3', 'h4')
     host_addrs = dict({'h1': h1.IP(), 'h2': h2.IP(), 'h3': h3.IP(), 'h4': h4.IP()})
     popens = dict()
     
     print("Starting servers h2 and h4")
-    popens[h2] = h2.popen(['iperf -s -p 5566 -w 16m'])
-    popens[h4] = h4.popen(['iperf -s -p 5566 -w 16m'])
+    popens[h2] = h2.popen('iperf -s -p 5566 -w 16m')
+    popens[h4] = h4.popen('iperf -s -p 5566 -w 16m')
     
     print("Starting clients h1 and h3")
-    popens[h1] = h1.popen(['iperf -c {0} -p 5566 -i 1 -w 16m -M 1460 -N -t 15 -y C > iperf_test_h1-h2_15ms.txt'.format(h2.IP())])
-    popens[h3] = h3.popen(['iperf -c {0} -p 5566 -i 1 -w 16m -M 1460 -N -t 15 -y C > iperf_test_h1-h2_15ms.txt'.format(h4.IP())])
+    popens[h1] = h1.popen('iperf -c {0} -p 5566 -i 1 -w 16m -M 1460 -N -t 15 -y C > iperf_test_h1-h2_15ms.txt'.format(h2.IP()), shell=True)
+    popens[h3] = h3.popen('iperf -c {0} -p 5566 -i 1 -w 16m -M 1460 -N -t 15 -y C > iperf_test_h1-h2_15ms.txt'.format(h4.IP()), shell=True)
     popens[h1].wait()
     popens[h3].wait()
-    
+
     popens[h2].terminate()
     popens[h4].terminate()
     popens[h2].wait()
