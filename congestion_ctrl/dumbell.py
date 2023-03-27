c28ecba8c11037e81bfe8182c8c6b6a07e02e94a
Jeff Tipps <jeff.tipps@ni.com>
2023-03-27 11:03:02 -0500

    fixed error

--- congestion_ctrl/dumbell.py
+++ congestion_ctrl/dumbell.py
@@ -13,7 +13,7 @@ class DumbellTopo(Topo):
 
 def simpleTest():
     "Create and test a simple network"
-    topo = SingleSwitchTopo()
+    topo = BumbellTopo()
     net = Mininet(topo)
     net.start()
     print( "Dumping host connections" )
