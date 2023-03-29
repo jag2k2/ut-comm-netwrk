33045b2ed0a305e55a5888c208b86f362f458e85
Jeff Tipps <jeff.tipps@ni.com>
2023-03-29 08:19:53 -0500

    removed json again

--- congestion_ctrl/dumbell.py
+++ congestion_ctrl/dumbell.py
@@ -59,14 +59,14 @@ def commTest(delay=2):
     popens[receiver1] = receiver1.popen('iperf3 -s -p 5566')
     popens[receiver2] = receiver2.popen('iperf3 -s -p 5566')
     
-    print("Starting Flow 2")
-    popens[source1] = source1.popen('iperf3 -c {0} -p 5566 -i 1 -M 1460 -N -t 200 -J > iperf3_data_flow1_{1}_{2}.txt'.format(receiver1.IP(), 'default', delay), shell=True)
+    print("Starting Flow 1")
+    popens[source1] = source1.popen('iperf3 -c {0} -p 5566 -i 1 -M 1460 -N -t 200 > iperf3_data_flow1_{1}_{2}.txt'.format(receiver1.IP(), 'default', delay), shell=True)
     
     print("Delaying Flow 2")
     sleep(100)
 
     print("Starting Flow 2")
-    popens[source2] = source2.popen('iperf3 -c {0} -p 5566 -i 1 -M 1460 -N -t 200 -J > iperf3_data_flow2_{1}_{2}.txt'.format(receiver2.IP(), 'default', delay), shell=True)
+    popens[source2] = source2.popen('iperf3 -c {0} -p 5566 -i 1 -M 1460 -N -t 200 > iperf3_data_flow2_{1}_{2}.txt'.format(receiver2.IP(), 'default', delay), shell=True)
     popens[source1].wait()
     popens[source2].wait()
 
