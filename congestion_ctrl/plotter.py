796dfeef7e0d3113ca98b44cf136f94fe70e3b4d
Roznik-J <roznikj@myumanitoba.ca>
2023-03-31 19:39:43 -0400

    Added support for multiple plots in the same plot

--- congestion_ctrl/plotter.py
+++ congestion_ctrl/plotter.py
@@ -26,33 +26,60 @@ def processData(anData):
             flag = -1
             timecandidate = -1
             cwndcandidate = -1
-            #print("except", " ", e) 
 
         if(isinstance(timecandidate, float) and isinstance(cwndcandidate,float) and flag == 1):
-            #print("ok")
             time.append(timecandidate)
             cwnd.append(cwndcandidate)
     
-    #print(len(time))
-    #print(len(cwnd))
     return(time,cwnd)
 
-def plotCwnd(Cwnd,time,lsFileName):
+def plotCwnd(MasterCwnd, MasterTime, PlotTitles, numofPlots, PlotName):
 
-    lsPlotName = lsFileName[0:len(lsFileName)-3]
-    lsPlotName += 'png'
-    print(lsPlotName)
-    if(len(Cwnd) != len(time)):
-        print("Size not the same")
-        return()
+    fig = plt.figure()
+    x = np.linspace(0, 2, 100)
+    fig, ax = plt.subplots()
+    ax.plot(x, x, label='linear')
+    ax.plot(x, x**2, label='quadratic')
+    ax.plot(x, x**3, label='cubic')
+    ax.set_xlabel('x label name')
+    ax.set_ylabel('y label name')
+    ax.set_title("My Plot")
+    ax.legend()
 
-    plt.plot(time,Cwnd)
-    plt.savefig(lsPlotName)
-
+    fig = plt.figure()
+    fig, ax = plt.subplots()
+    ax.set_xlabel('time')
+    ax.set_ylabel('CWND (KBytes)')
+    for i in range(0, int(numofPlots)):
+        ax.plot(MasterTime[i], MasterCwnd[i], label = PlotTitles[i])
+    ax.set_title(PlotName[0:len(PlotName)-4])
+    ax.legend()
+    plt.savefig(PlotName)
 
 if __name__ == '__main__':
+    """
+    usage :
+    python3 plotter.py plotNameImg numOfPlots file1 file1_Title file2 file2_Title ... fileN fileN_Title
+    example: 
+    python3 plotter.py test1 2 file1.txt dat-1 file2.txt dat-2
+    """
     argv = sys.argv[1:]
-    lsFileName = argv[0]
-    Data = getData(lsFileName)
-    time,cwnd = processData(Data)
-    plotCwnd(cwnd,time,lsFileName)
+
+    PlotName = argv[0]
+    numofPlots = argv[1]
+    MasterTime = []
+    MasterCwnd = []
+    PlotTitles = []
+    i = 0
+    while(i < 2*int(numofPlots)):
+        Data = getData(argv[i+2])
+        i += 1
+        PltName = argv[i+2]
+        i += 1
+        time,cwnd = processData(Data)
+        MasterTime += [time]
+        MasterCwnd += [cwnd]
+        PlotTitles += [PltName]
+        
+    plotCwnd(MasterCwnd, MasterTime, PlotTitles, numofPlots, PlotName)
+    
