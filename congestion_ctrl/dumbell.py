1949c7a61ce4bf167843a0486fdc81bb4cf0597a
Jeff Tipps <jeff.tipps@ni.com>
2023-04-04 20:34:10 -0500

    parameterized delays

--- congestion_ctrl/dumbell.py
+++ congestion_ctrl/dumbell.py
@@ -49,7 +49,7 @@ class DumbellTopo(Topo):
         self.addLink(access_switch2, receiver1, cls=TCLink, **host_params)
         self.addLink(access_switch2, receiver2, cls=TCLink, **host_params)
 
-def commTest(delay=2):
+def commTest(delay=2, algo='cubic'):
     topo = DumbellTopo(delay=delay)
     net = Mininet(topo)
     net.start()
@@ -62,14 +62,14 @@ def commTest(delay=2):
     popens[receiver2] = receiver2.popen('iperf3 -s -p 5566')
     
     print("Starting Flow 1")
-    popens[source1] = source1.popen('iperf3 -c {0} -p 5566 -i 1 -C cubic -t 200 > iperf3_data_flow1_{1}_{2}.txt'.format(receiver1.IP(), 'default', delay), shell=True)
+    popens[source1] = source1.popen('iperf3 -c {0} -p 5566 -i 1 -C {1} -t 2000 > iperf3_data_flow1_{1}_{2}.txt'.format(receiver1.IP(), algo, delay), shell=True)
     #popens[source1] = source1.popen('iperf3 -c {0} -p 5566 -V -4 -i 1 -C cubic -f m -d -t 20 > iperf3_data_flow1_{1}_{2}.txt'.format(receiver1.IP(), 'default', delay), shell=True)
 
     print("Delaying Flow 2")
-    sleep(100)
+    sleep(250)
 
     print("Starting Flow 2")
-    popens[source2] = source2.popen('iperf3 -c {0} -p 5566 -i 1 -C cubic -t 100 > iperf3_data_flow2_{1}_{2}.txt'.format(receiver2.IP(), 'default', delay), shell=True)
+    popens[source2] = source2.popen('iperf3 -c {0} -p 5566 -i 1 -C {1} -t 1750 > iperf3_data_flow2_{1}_{2}.txt'.format(receiver2.IP(), algo, delay), shell=True)
     #popens[source2] = source2.popen('iperf3 -c {0} -p 5566 -V -4 -i 1 -C cubic -f m -d -t 10 > iperf3_data_flow2_{1}_{2}.txt'.format(receiver2.IP(), 'default', delay), shell=True)
     popens[source1].wait()
     popens[source2].wait()
@@ -82,4 +82,6 @@ def commTest(delay=2):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    commTest(21)
+    delays = [21,81,162]
+    for delay in delays
+        commTest(delay, 'cubic')
