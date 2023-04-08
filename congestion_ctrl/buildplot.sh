904a191c6fd0328308a7e7091ddbe7232268fab5
Roznik-J <roznikj@myumanitoba.ca>
2023-04-08 15:05:28 -0400

    Plotted all the data

---
+++ congestion_ctrl/buildplot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 plotter.py test2 2 iperf3_data_flow1_cubic_162.txt flow-1 iperf3_data_flow2_cubic_162.txt flow-2
--- congestion_ctrl/plotter.py
+++ congestion_ctrl/plotter.py
@@ -11,48 +11,75 @@ def getData(asFileName):
             i += 1
     return(Data)
 
-def processData(anData):
+def getModifier(name):
+    if(name == "bits/sec"): return(1/1000000)
+    if(name == "Bytes" or name == "Kbits/sec"): return(1/1000)
+    if(name == "KBytes" or name == "Mbits/sec"): return(1)
+    if(name == "MBytes" or name == "Gbits/sec"): return(1000)
+    else:
+        print("Name not Found :", name)
+        return(0)
+
+def processData(anData, time_shift):
+    # TODO, Add support to reive information regarding Throughput
+    # TODO, Add support to change CWND data to amount of packets - looks good so far
+    # TODO, Add support to do Unit Conversion - looks good so far
+    pktSize = 1500
+    ByteUnit = ["bytes", "KBytes", "MBytes"]
+    BitUnit = ["bits/sec", "Kbits/sec", "Mbits/sec"]
     time = []
+    band = []
     cwnd = []
     for i in range(0, len(anData)):
         line = anData[i]
         flag = 1
         try:
             timecandidate = line[2].split("-")[0]
+            bandcandidate = line[6]
+            bandUnit = line[7]
             cwndcandidate = line[9]
+            cwndUnit = line[10]
             timecandidate = float(timecandidate)
-            cwndcandidate = float(cwndcandidate)
+            bandcandidate = float(bandcandidate) * getModifier(bandUnit)
+            cwndcandidate = (1000*(float(cwndcandidate) * getModifier(cwndUnit))/pktSize)
         except Exception as e:
             flag = -1
             timecandidate = -1
+            bandcandidate = -1
             cwndcandidate = -1
 
-        if(isinstance(timecandidate, float) and isinstance(cwndcandidate,float) and flag == 1):
-            time.append(timecandidate)
+        if(isinstance(timecandidate, float) and isinstance(cwndcandidate,float) and flag == 1 and bandcandidate > 0 and cwndcandidate > 0):
+            time.append(timecandidate + time_shift)
+            band.append(bandcandidate)
             cwnd.append(cwndcandidate)
     
-    return(time,cwnd)
+    return(time,cwnd,band)
+
+# TODO Add a function that adds support to plot the throughput.
+
+def plotThroughput(MasterTHPT, MasterTime, PlotTitles, numofPlots, PlotName):
+    fig = plt.figure()
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Throughput MBits/sec')
+    for i in range(0, int(numofPlots)):
+        ax.plot(MasterTime[i], MasterTHPT[i], label = PlotTitles[i])
+    #ax.set_title(PlotName[0:len(PlotName)-4])
+    ax.set_title("Throughput for Cubic Algorithm, delay = 162ms")
+    ax.legend()
+    plt.savefig(PlotName)
+    
 
 def plotCwnd(MasterCwnd, MasterTime, PlotTitles, numofPlots, PlotName):
 
     fig = plt.figure()
-    x = np.linspace(0, 2, 100)
     fig, ax = plt.subplots()
-    ax.plot(x, x, label='linear')
-    ax.plot(x, x**2, label='quadratic')
-    ax.plot(x, x**3, label='cubic')
-    ax.set_xlabel('x label name')
-    ax.set_ylabel('y label name')
-    ax.set_title("My Plot")
-    ax.legend()
-
-    fig = plt.figure()
-    fig, ax = plt.subplots()
-    ax.set_xlabel('time')
-    ax.set_ylabel('CWND (KBytes)')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('CWND (Packets)')
     for i in range(0, int(numofPlots)):
         ax.plot(MasterTime[i], MasterCwnd[i], label = PlotTitles[i])
-    ax.set_title(PlotName[0:len(PlotName)-4])
+    #ax.set_title(PlotName[0:len(PlotName)-4])
+    ax.set_title("CWND for Cubic Algorithm, delay = 162ms")
     ax.legend()
     plt.savefig(PlotName)
 
@@ -69,17 +96,23 @@ if __name__ == '__main__':
     numofPlots = argv[1]
     MasterTime = []
     MasterCwnd = []
+    MasterBAND = []
     PlotTitles = []
     i = 0
-    while(i < 2*int(numofPlots)):
-        Data = getData(argv[i+2])
-        i += 1
-        PltName = argv[i+2]
-        i += 1
-        time,cwnd = processData(Data)
-        MasterTime += [time]
-        MasterCwnd += [cwnd]
-        PlotTitles += [PltName]
+    Data1 = getData(argv[2])
+    PltName1 = argv[3]
+    time1,cwnd1,band1 = processData(Data1,0)
+    MasterTime += [time1]
+    MasterCwnd += [cwnd1]
+    MasterBAND += [band1]
+    PlotTitles += [PltName1]
+    Data2 = getData(argv[4])
+    PltName2 = argv[5]
+    time2,cwnd2,band2 = processData(Data2,100)
+    MasterTime += [time2]
+    MasterCwnd += [cwnd2]
+    MasterBAND += [band2]
+    PlotTitles += [PltName2]
         
-    plotCwnd(MasterCwnd, MasterTime, PlotTitles, numofPlots, PlotName)
-    
+    plotCwnd(MasterCwnd, MasterTime, PlotTitles, numofPlots, "Cubic-CWND-162")
+    plotThroughput(MasterBAND, MasterTime, PlotTitles, numofPlots, "Cubic-THPT-162")
